2765f77712fdad3f901ffb70c68cc6e70a2a108f
jun <caoweijun4451@gmail.com>
2025-01-03 16:53:16 +0800

    jun
    先把檔案創完
    pojo暫時寫完
    dao寫了ComOrderDao

---
+++ src/main/java/web/companion/dao/impl/ComOrderDaoImpl.java
@@ -0,0 +1,5 @@
+package web.companion.dao.impl;
+
+public class ComOrderDaoImpl {
+
+}
---
+++ src/main/java/web/companion/pojo/ComSkill.java
@@ -0,0 +1,9 @@
+package web.companion.pojo;
+
+import lombok.Data;
+
+@Data
+public class ComSkill {
+	private String expertiseNo;
+	private String expertiseLabel;
+}
