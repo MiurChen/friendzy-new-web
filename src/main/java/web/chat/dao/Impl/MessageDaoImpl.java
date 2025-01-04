72ed358fa0925619912c5cda121970d107e02829
Miu <eternal.sea.memory@gmail.com>
2025-01-04 16:44:34 +0800

    chatBean

---
+++ src/main/java/web/chat/dao/Impl/MessageDaoImpl.java
@@ -0,0 +1,40 @@
+package web.chat.dao.Impl;
+
+import java.util.List;
+
+import web.chat.dao.MessageDao;
+import web.chat.pojo.Message;
+
+public class MessageDaoImpl extends MessageDao{
+
+	@Override //沒有這功能
+	public int update(Message item) throws Exception {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override //沒有這功能
+	public int deleteBy(String id) throws Exception {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int insert(Message item) throws Exception {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public Message seleteBy(String id) throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Message> seleteAll() throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
---
+++ src/main/java/web/chat/pojo/ChatRoom.java
@@ -0,0 +1,10 @@
+package web.chat.pojo;
+
+import lombok.Data;
+
+@Data
+public class ChatRoom {
+	private Integer room_no;
+	private Integer room_user_one;
+	private Integer room_user_two;
+}
